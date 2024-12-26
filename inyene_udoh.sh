4d03d3ebe522d5f2ef9b8806af9c7497eb6393db
Inyene Udoh <paulinyene@gmail.com>
2024-12-26 15:22:44 +0100

    Bash Script Assignment DONE

---
+++ inyene_udoh.sh
@@ -0,0 +1,30 @@
+
+#!/bin/bash
+who_am_i() {
+	echo "Hello! My full name is Inyene Paul Udoh"
+}
+
+bio() {
+	echo "I am an aspiring cloud engineer currently enrolled at AltSchool Africa. Born on 14th January, I hold a Masters degree in Computer Science from the prestigious Babcock University in Ogun State, Nigeria. With a keen interest in cloud technologies and a dedication to mastering the complexities of cloud infrastructure, Inyene is on a path to becoming a proficient and innovative professional in the field."
+}
+
+year_in_tech() {
+	echo "I have been in tech for 2 years as a Product Manager"
+}
+dream_job() {
+	echo "My dream job is to work to any of the big 4 tech companies (Google, Meta, Apple, Amazon)"
+}
+
+goals() {
+	echo "My goals are as follows:
+	1. Career Advancement: You might aim to advance in your current career or transition into a cloud engineering role, which is in high demand across various industries.
+2. Mastering Cloud Technologies: A key goal could be to develop a deep understanding of cloud platforms like AWS, Azure, or Google Cloud, and to become proficient in designing, deploying, and managing cloud infrastructure.
+3. Building Scalable Solutions: You may want to learn how to design and implement scalable, reliable, and cost-effective cloud solutions that can support business growth and efficiency.
+4. Certifications and Credibility: Achieving industry-recognized certifications, such as AWS Certified Solutions Architect or Google Cloud Professional Cloud Architect, might be a goal to validate your skills and increase your marketability."
+}
+
+who_am_i
+bio
+year_in_tech
+dream_job
+goals
